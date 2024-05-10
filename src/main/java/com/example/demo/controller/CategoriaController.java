98c6e8d0a12d5159257b429ff1a1b0e478635cd0
DaniNishikino <nimguem1453@gmail.com>
2024-05-10 11:04:12 -0300

    I created documentation using the Swagger plugin, and described the methods of the category entity.

--- src/main/java/com/example/demo/controller/CategoriaController.java
+++ src/main/java/com/example/demo/controller/CategoriaController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entities.Categoria;
 import com.example.demo.service.CategoriaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,12 @@ public class CategoriaController {
     CategoriaService categoriaService;
 
     @GetMapping
+    @Operation(summary = "Listar todas as categorias", description = "Listagem de Categorias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A requisição foi executada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar a API"),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.")})
     public ResponseEntity<List<Categoria>> getAll() {
         List<Categoria> categorias = categoriaService.getAll();
         if (!categorias.isEmpty()) {
@@ -26,6 +35,12 @@ public class CategoriaController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Coletar um elemento da API por um ID", description = "Listagem de Categorias por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A requisição foi executada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar a API"),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.")})
     public ResponseEntity<Categoria> getById(@PathVariable Integer id){
         Categoria categoria = categoriaService.getById(id);
         if (categoria != null){
@@ -37,11 +52,23 @@ public class CategoriaController {
 
 
     @PostMapping
+    @Operation(summary = "Salvar uma nova Categoria", description = "Salvar Categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A requisição foi executada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar a API"),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.")})
     public ResponseEntity<Categoria> saveCategoria(@RequestBody Categoria categoria){
         return new ResponseEntity<>(categoriaService.saveCategoria(categoria), HttpStatus.CREATED);
 
     }
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar uma Categoria existente", description = "Atualizar Categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A requisição foi executada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar a API"),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.")})
     public ResponseEntity<Categoria> updateCategoria(@PathVariable Integer id, @RequestBody Categoria categoria){
         Categoria categoriaAtualizada = categoriaService.updateCategoria(id, categoria);
         if (categoriaAtualizada != null){
@@ -52,6 +79,12 @@ public class CategoriaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar uma categoria por um ID fornecido", description = "Deletar categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A requisição foi executada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar a API"),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.")})
     public ResponseEntity<Boolean> deleteCategoria(@PathVariable Integer id){
         if (categoriaService.deleteCategoria(id)){
             return new ResponseEntity<>(true, HttpStatus.OK);
